fa68e986041197cba6e4400a6b0c70d4ba1c755a
Floriane HJ <fjulia.dev@gmail.com>
2024-11-06 10:53:09 +0100

    update login.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,14 +4,15 @@ const Login = () => {
             <h1 className="font-secondary text-4xl pb-10">
                 Connexion / Inscription
             </h1>
-            <div className="flex flex-row gap-10">
+            <div className="flex flex-row gap-10 w-3/4">
                 {/* Formulaire de connexion */}
-                <div className="bg-background2/70 p-5 rounded-md w-1/2">
-                    <span>Vous avez déjà un compte ?</span>
+                <div className="bg-background2/70 p-5 rounded-lg w-1/2 ">
+                    <span className="italic">Vous avez déjà un compte ?</span>
                     <h2 className="text-2xl font-semibold mb-4">
                         Connectez-vous !
                     </h2>
-                    <form>
+
+                    <form className="flex flex-col justify-center py-4 ">
                         <div className="mb-4">
                             <label htmlFor="username" className="block mb-1">
                                 Identifiant ou adresse e-mail
@@ -20,7 +21,7 @@ const Login = () => {
                                 type="text"
                                 id="username"
                                 placeholder="Votre identifiant ou e-mail"
-                                className="border border-gray-300 rounded px-3 py-2 w-full"
+                                className=" outline-none rounded px-3 py-2 w-full"
                             />
                         </div>
                         <div className="mb-4">
@@ -31,25 +32,27 @@ const Login = () => {
                                 type="password"
                                 id="password"
                                 placeholder="Votre mot de passe"
-                                className="border border-gray-300 rounded px-3 py-2 w-full"
+                                className="outline-none rounded px-3 py-2 w-full"
                             />
                         </div>
-                        <span className="text-sm text-gray-500 cursor-pointer hover:underline">
-                            Identifiant/mot de passe oublié ?
+                        <span className="text-sm italic cursor-pointer hover:underline">
+                            Identifiant ou mot de passe oublié ?
                         </span>
-                        <button type="submit" className="btn">
+                        <button type="submit" className="btn mt-12">
                             Se connecter
                         </button>
                     </form>
                 </div>
 
                 {/* Formulaire d'inscription */}
-                <div className="bg-background2/70 p-5 rounded-md w-1/2">
-                    <span>Vous n'avez pas encore de compte ?</span>
+                <div className="bg-background2/70 p-5 rounded-lg w-1/2 ">
+                    <span className="italic">
+                        Vous n'avez pas encore de compte ?
+                    </span>
                     <h2 className="text-2xl font-semibold mb-4">
                         Inscrivez-vous !
                     </h2>
-                    <form>
+                    <form className="flex flex-col justify-center  ">
                         <div className="mb-4">
                             <label
                                 htmlFor="signup-email"
@@ -61,7 +64,7 @@ const Login = () => {
                                 type="email"
                                 id="signup-email"
                                 placeholder="Votre adresse e-mail"
-                                className="border border-gray-300 rounded px-3 py-2 w-full"
+                                className="outline-none rounded px-3 py-2 w-full"
                             />
                         </div>
                         <div className="mb-4">
@@ -75,7 +78,7 @@ const Login = () => {
                                 type="password"
                                 id="signup-password"
                                 placeholder="Créez un mot de passe"
-                                className="border border-gray-300 rounded px-3 py-2 w-full"
+                                className="outline-none rounded px-3 py-2 w-full"
                             />
                         </div>
                         <div className="mb-4">
@@ -89,9 +92,10 @@ const Login = () => {
                                 type="password"
                                 id="confirm-password"
                                 placeholder="Confirmez votre mot de passe"
-                                className="border border-gray-300 rounded px-3 py-2 w-full"
+                                className="outline-none rounded px-3 py-2 w-full"
                             />
                         </div>
+
                         <button type="submit" className="btn ">
                             S'inscrire
                         </button>
